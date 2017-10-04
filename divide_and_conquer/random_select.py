e24931212c4b29c0df2e6e5a1c5ba95c46f1eba4
ziqi-liu <32336495+ziqi-liu@users.noreply.github.com>
2017-10-03 19:19:07 -0500

    Update random_select.py

--- divide_and_conquer/random_select.py
+++ divide_and_conquer/random_select.py
@@ -8,7 +8,6 @@ import random
 def select(a,k): #select the kth statistic in list a in O(n)
     l = len(a)
     if l == 1:
-        print(0)
         return a[0]
     p = random.randint(0,l-1)
     a[0],a[p] = a[p],a[0]
@@ -19,12 +18,9 @@ def select(a,k): #select the kth statistic in list a in O(n)
             i += 1
         j += 1
     if i == k:
-        print(0)
         return a[0]
     if i < k:
-        print(a[i:],k-i)
         return select(a[i:],k-i)
-    print(a[1:i],k)
     return select(a[1:i],k)
     
 
